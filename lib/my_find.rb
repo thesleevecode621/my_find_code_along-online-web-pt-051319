e7a645793ffc5b141d207d7b064307dbc9407242
boruch d <dubywider@gmail.com>
2019-05-16 16:39:48 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,17 +1,6 @@
 require 'pry'
 
 def my_find(collection)
-i = 0 
-while i < 
-collection.length
-return
-collection[i]
-if
-  yield (collection[i])
-i = i + 1
- end
-end
-def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
